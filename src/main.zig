9af5cc876eabf5449e53da88b9d91923cc65c620
Colin Chan <colin+git@lumeh.org>
2024-10-04 21:09:31 -0700

    Multi-select

--- src/main.zig
+++ src/main.zig
@@ -1,74 +1,78 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 
 const rl = @import("raylib");
 const Vector2 = rl.Vector2;
 
 const Ball = struct {
     pos: Vector2,
+    radius: f32,
     velocity: Vector2,
     spin: Vector2,
 
     fn init(x: f32, y: f32) Ball {
         return Ball{
             .pos = Vector2.init(x, y),
+            .radius = 12,
             .velocity = Vector2.zero(),
             .spin = Vector2.zero(),
         };
     }
-    fn x_i32(self: Ball) i32 {
-        return @intFromFloat(self.pos.x);
-    }
-    fn y_i32(self: Ball) i32 {
-        return @intFromFloat(self.pos.y);
-    }
 };
 
 const Cursor = struct {
-    current_ball: u8,
     state: CursorState,
+    current_ball: u8,
+    pos: Vector2,
+    angle: Vector2,
+    radius: f32,
 
-    fn init(ball: u8) Cursor {
+    fn init(ball_index: u8, pos: Vector2) Cursor {
         return Cursor{
-            .current_ball = ball,
             .state = .inactive,
+            .current_ball = ball_index,
+            .pos = pos,
+            .angle = Vector2.init(1, 0),
+            .radius = 24,
         };
     }
 };
 
 const CursorState = enum {
     inactive,
-    aiming,
+    active,
 };
 
 const Hole = struct {
     pos: Vector2,
-    size: u8,
+    radius: f32,
 
     fn init(x: f32, y: f32) Hole {
         return Hole{
             .pos = Vector2.init(x, y),
-            .size = 24,
+            .radius = 24,
         };
-    }
-    fn x_i32(self: Hole) i32 {
-        return @intFromFloat(self.pos.x);
-    }
-    fn y_i32(self: Hole) i32 {
-        return @intFromFloat(self.pos.y);
     }
 };
 
+const GameMode = enum {
+    selecting,
+    aiming,
+};
+
 const GameState = struct {
-    balls: std.ArrayList(Ball),
-    cursors: std.ArrayList(Cursor),
-    holes: std.ArrayList(Hole),
+    mode: GameMode = .selecting,
+
+    balls: ArrayList(Ball),
+    cursors: ArrayList(Cursor),
+    holes: ArrayList(Hole),
 
     fn init(allocator: Allocator) GameState {
         return GameState{
-            .balls = std.ArrayList(Ball).init(allocator),
-            .cursors = std.ArrayList(Cursor).init(allocator),
-            .holes = std.ArrayList(Hole).init(allocator),
+            .balls = ArrayList(Ball).init(allocator),
+            .cursors = ArrayList(Cursor).init(allocator),
+            .holes = ArrayList(Hole).init(allocator),
         };
     }
 
@@ -87,90 +91,140 @@ pub fn main() !void {
 
     const allocator = arena.allocator();
 
-    var pastStates = std.ArrayList(GameState).init(allocator);
-    var futureStates = std.ArrayList(GameState).init(allocator);
+    var pastStates = ArrayList(GameState).init(allocator);
+    var futureStates = ArrayList(GameState).init(allocator);
     var state = GameState.init(allocator);
+
     try state.balls.append(Ball.init(50, 50));
-    try state.cursors.append(Cursor.init(0));
+    try state.balls.append(Ball.init(500, 100));
+    try state.cursors.append(Cursor.init(0, Vector2.init(50, 50)));
+    try state.cursors.append(Cursor.init(1, Vector2.init(50, 50)));
     try state.holes.append(Hole.init(600, 400));
+
+    var last_mode: GameMode = state.mode;
+    var hovered_cursor_i: ?usize = null;
 
     rl.setConfigFlags(rl.ConfigFlags{ .vsync_hint = true });
     rl.initWindow(800, 600, "test");
     defer rl.closeWindow();
 
     main: while (!rl.windowShouldClose()) {
+        if (state.mode != last_mode) {
+            std.debug.print("Mode changed: {}", .{state.mode});
+            last_mode = state.mode;
+        }
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.black);
         rl.drawText("Golf!", 710, 10, 32, rl.Color.light_gray);
 
-        var commands = try getCommands(allocator);
-        defer commands.deinit();
-        for (commands.items) |command| {
-            switch (command) {
-                .quit => break :main,
-                .undo => {
-                    if (pastStates.popOrNull()) |prev| {
-                        try futureStates.append(state);
-                        state = prev;
-                    }
-                },
-                .redo => {
-                    if (futureStates.popOrNull()) |fut| {
-                        try pastStates.append(state);
-                        state = fut;
-                    }
-                },
-                .act => {
-                    for (state.cursors.items) |*cursor| {
-                        switch (cursor.state) {
-                            .inactive => {
-                                cursor.state = .aiming;
-                            },
-                            .aiming => {
-                                cursor.state = .inactive;
-                                try pastStates.append(state);
-                                state = try state.clone();
-                                var ball = &state.balls.items[cursor.current_ball];
-                                const mouse_pos = rl.getMousePosition();
-                                const dir = rl.math.vector2Scale(mouse_pos.subtract(ball.pos), 0.04);
-                                ball.velocity = ball.velocity.add(dir);
-                            },
-                        }
-                    }
-                },
+        const commands = getCommands();
+        if (commands.quit) {
+            break :main;
+        }
+        if (commands.undo) {
+            if (pastStates.popOrNull()) |prev| {
+                try futureStates.append(state);
+                state = prev;
             }
         }
-        // -- KEYS -------------------------------
-        // act
+        if (commands.redo) {
+            if (futureStates.popOrNull()) |fut| {
+                try pastStates.append(state);
+                state = fut;
+            }
+        }
+
+        switch (state.mode) {
+            .selecting => {
+                if (commands.act) {
+                    // Set cursor active
+                    if (hovered_cursor_i) |i| {
+                        var cursor = &state.cursors.items[i];
+                        cursor.state = .active;
+                    }
+                } else if (commands.shift_act) {
+                    // Toggle cursor state
+                    if (hovered_cursor_i) |i| {
+                        var cursor = &state.cursors.items[i];
+                        cursor.state = if (cursor.state == .inactive) .active else .inactive;
+                    }
+                } else {
+                    // Update hovered cursor
+                    hovered_cursor_i = null;
+                    const mousepos = rl.getMousePosition();
+                    var min_distance: f32 = 50;
+                    for (state.cursors.items, 0..) |cursor, i| {
+                        const distance = cursor.pos.subtract(mousepos).length();
+                        if (distance < min_distance) {
+                            min_distance = distance;
+                            hovered_cursor_i = i;
+                        }
+                    }
+                }
+                // Only switch to aiming if the click was actually on a cursor
+                if (commands.act and hovered_cursor_i != null) {
+                    state.mode = .aiming;
+                }
+            },
+            .aiming => {
+                if (commands.act) {
+                    try pastStates.append(state);
+                    state = try state.clone();
+                    for (state.cursors.items) |*cursor| {
+                        if (cursor.state == .active) {
+                            cursor.state = .inactive;
+                            var ball = &state.balls.items[cursor.current_ball];
+                            const mouse_pos = rl.getMousePosition();
+                            const dir = rl.math.vector2Scale(mouse_pos.subtract(ball.pos), 0.04);
+                            ball.velocity = ball.velocity.add(dir);
+                        }
+                    }
+                    state.mode = .selecting;
+                } else if (commands.shift_act) {
+                    rl.clearBackground(rl.Color.red);
+                }
+            },
+        }
 
         // -- PHYSICS ----------------------------
         process_physics(state.balls);
 
+        // Align cursors to balls
+        for (state.cursors.items) |*cursor| {
+            cursor.pos = state.balls.items[cursor.current_ball].pos;
+        }
+
         // -- DRAWING ----------------------------
         // holes
         for (state.holes.items) |hole| {
-            rl.drawCircle(hole.x_i32(), hole.y_i32(), @floatFromInt(hole.size), rl.Color.init(20, 80, 20, 255));
+            rl.drawCircleV(hole.pos, hole.radius, rl.Color.init(20, 80, 20, 255));
         }
         // balls
         for (state.balls.items) |ball| {
-            rl.drawCircle(ball.x_i32(), ball.y_i32(), 12, rl.Color.blue);
+            rl.drawCircleV(ball.pos, ball.radius, rl.Color.blue);
         }
         // cursors
-        for (state.cursors.items) |cursor| {
+        for (state.cursors.items, 0..) |cursor, i| {
             const ball = state.balls.items[cursor.current_ball];
-            rl.drawCircleLines(ball.x_i32(), ball.y_i32(), 24, switch (cursor.state) {
-                .inactive => rl.Color.green,
-                .aiming => rl.Color.orange,
-            });
+            const color = if (i == hovered_cursor_i or cursor.state == .active) rl.Color.orange else rl.Color.green;
+            rl.drawCircleLinesV(ball.pos, 24, color);
+            if (state.mode == .aiming and cursor.state == .active) {
+                rl.drawTriangle(
+                    ball.pos.add(cursor.angle.normalize().scale(cursor.radius).rotate(0.2)),
+                    ball.pos.add(cursor.angle.normalize().scale(cursor.radius).scale(1.4)),
+                    ball.pos.add(cursor.angle.normalize().scale(cursor.radius).rotate(-0.2)),
+                    color,
+                );
+            }
         }
         // mouse indicator
         rl.drawCircle(rl.getMouseX(), rl.getMouseY(), 8, rl.Color.gold);
     }
 }
 
-fn process_physics(balls: std.ArrayList(Ball)) void {
+fn process_physics(balls: ArrayList(Ball)) void {
     for (balls.items) |*ball| {
         ball.pos = ball.pos.add(ball.velocity.clampValue(0, 10.0));
         ball.velocity = ball.velocity.scale(0.96);
@@ -180,23 +234,32 @@ fn process_physics(balls: std.ArrayList(Ball)) void {
     }
 }
 
-const Command = enum {
-    quit,
-    act,
-    undo,
-    redo,
+const Commands = struct {
+    quit: bool = false,
+    undo: bool = false,
+    redo: bool = false,
+    act: bool = false,
+    shift_act: bool = false,
 };
 
-fn getCommands(allocator: Allocator) !std.ArrayList(Command) {
-    var commands = std.ArrayList(Command).init(allocator);
+fn getCommands() Commands {
+    var commands = Commands{};
     if (rl.isKeyPressed(.key_q)) {
-        try commands.append(.quit);
+        commands.quit = true;
     }
     if (rl.isKeyPressed(.key_u) or rl.isKeyPressed(.key_z)) {
-        try commands.append(if (isShiftDown()) .redo else .undo);
+        if (isShiftDown()) {
+            commands.redo = true;
+        } else {
+            commands.undo = true;
+        }
     }
     if (rl.isKeyPressed(.key_space) or rl.isMouseButtonPressed(.mouse_button_left)) {
-        try commands.append(.act);
+        if (isShiftDown()) {
+            commands.shift_act = true;
+        } else {
+            commands.act = true;
+        }
     }
     return commands;
 }
